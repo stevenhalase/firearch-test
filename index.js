aa33e858c4985e05f35358a04f649665a19dbdf6
Steven Halase <steven.halase@gmail.com>
2018-12-21 23:02:01 -0700

    Added test models

--- index.js
+++ index.js
@@ -2,27 +2,40 @@ const firearch = require('../firearch');
 const firebaseConfig = require('./firebaseConfig');
 
 const Model1 = require('./models/Model1');
+const Model2 = require('./models/Model2');
+
+const crypto = require('crypto');
 
 firearch.connect(firebaseConfig, { timestampsInSnapshots: true });
 
-Model1.save({
-  title: 'A title',
-  subtitle: 'A subtitle',
-  something: 'is something'
-})
-.then(response => {
-  console.log('RESPONSE: ', response);
-})
-.catch(error => {
-  console.log('ERROR: ', error);
-})
+// Model1.save({
+//   title: 'Some title',
+//   subtitle: 'Some subtitle'
+// })
+// .then(response => {
+//   console.log('RESPONSE: ', response);
+// })
+// .catch(error => {
+//   console.log('ERROR: ', error);
+// })
 
-// Model1.findById('ad311ea7b95d128f1af123632433e707')
-//   .then(response => {
-//     console.log('RESPONSE: ', response);
-//   })
-//   .catch(error => {
-//     console.log('ERROR: ', error);
-//   })
+// Model2.save({
+//   name: 'A name',
+//   referenceExample: '4dd423eb052a95cdb407001b3ad03c5f'
+// })
+// .then(response => {
+//   console.log('RESPONSE: ', response);
+// })
+// .catch(error => {
+//   console.log('ERROR: ', error);
+// })
+
+Model2.findById('849cbd68f7a40b1db2229506bd3e576b')
+  .then(response => {
+    // console.log('RESPONSE: ', response);
+  })
+  .catch(error => {
+    // console.log('ERROR: ', error);
+  })
 
 
--- models/Model2.js
+++ models/Model2.js
@@ -1,11 +1,17 @@
 const firearch = require('../../firearch');
 const Schema = firearch.Schema;
 
-const Model1 = require('./Model1');
-
 const Model2Schema = new Schema({
   name: String,
-  packId: { ref: Model1 }
+  referenceExample: { ref: 'Model1' }
 });
+
+const autoPopulate = function(next) {
+  this.populate({ path: 'referenceExample', model: 'Model1' });
+  next();
+};
+
+Model2Schema
+  .pre('findById', autoPopulate);
 
 module.exports = firearch.model('Model2', Model2Schema);
